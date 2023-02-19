e24fa3abf515a77099e39d8e8c83390d62176e06
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 16:19:24 -0500

    Second Commit

--- src/MetricConverter.java
+++ src/MetricConverter.java
@@ -4,10 +4,6 @@ public class MetricConverter {
     public static void main(String[] args) {
 
         //Declare variables
-        double meters;
-        double miles;
-        double feet;
-        double inches;
 
         //Create scanner object
         Scanner input = new Scanner(System.in);
@@ -15,9 +11,9 @@ public class MetricConverter {
         System.out.print("Please give a measurement in meters: ");
         double meters = input.nextDouble(); //user input for needed variable
         //Formulas to figure meters to the variable needed
-        miles = (meters/1609)
-        feet = (meters/0.305)
-        inches = (meters*39.37)
+        double miles = (meters/1609)
+        double feet = (meters/0.305)
+        double inches = (meters*39.37)
 
         System.out.print("Converted to miles: " + miles);
         System.out.print("Converted to miles: " + feet);
