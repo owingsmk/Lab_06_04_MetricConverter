0ce48bd8745b22b460a5a6c52682a46fb593d102
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 16:07:03 -0500

    Initial commit

---
+++ src/MetricConverter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MetricConverter {
+    public static void main(String[] args) {
+
+        //Declare variables
+        double meters;
+        double miles;
+        double feet;
+        double inches;
+
+        //Create scanner object
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please give a measurement in meters: ");
+
+    }
+}
